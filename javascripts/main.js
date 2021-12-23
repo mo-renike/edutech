45609d00c3210fae9eead931343888bfd81e0835
Morenike Oyewole <65955563+mo-renike@users.noreply.github.com>
2021-12-23 16:34:25 +0100

    updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 // Sticky Navbar
-const navbar = document.querySelector("header");
+const navbar = document.querySelector("nav");
 let sticky = navbar.offsetTop;
 const navbarScroll = () => {
   if (window.pageYOffset > sticky) {
@@ -11,29 +11,3 @@ const navbarScroll = () => {
 window.onscroll = navbarScroll;
 
 
-console.log(navbar);
-
-//navigation 
-
-const nav = document.querySelector('.nav')
-const toggler = document.querySelector('.toggler')
-const close = document.querySelector('.nav__close')
-
-toggler.addEventListener('click', () =>{
-nav.classList.add('show')
-})
-close.addEventListener('click', () =>{
-nav.classList.remove('show')
-})
-
-
-//preloader and pop-up text
-window.onload = function () {
-  document.querySelector(".preloader").style.display = "none";
-};
-
-//swiper
-var swiper = new Swiper(".mySwiper", {
-  effect: "cards",
-  grabCursor: true,
-});
